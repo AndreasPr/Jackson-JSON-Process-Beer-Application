18f5f2e0f00c5e4cdf1a7918e0fb73ac94a9d1ce
Andreas Priftis <31467938+AndreasPr@users.noreply.github.com>
2020-10-25 20:40:05 -0400

    Jackson Property Naming Strategy - Change to SNAKE_CASE naming strategy of the properties in the JSON Object during serialization

---
+++ src/test/java/com/andreasgroup/jacksonjsonprocessbeerapplication/model/BeerDtoSnakeCaseTest.java
@@ -0,0 +1,27 @@
+package com.andreasgroup.jacksonjsonprocessbeerapplication.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.JsonTest;
+import org.springframework.test.context.ActiveProfiles;
+
+/**
+ * Created on 25/Oct/2020 to jackson-json-process-beer-application
+ */
+@ActiveProfiles("snake")
+@JsonTest
+public class BeerDtoSnakeCaseTest extends BaseTest{
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void testSnake() throws JsonProcessingException {
+        BeerDto beerDto = getDto();
+
+        String jsonData = objectMapper.writeValueAsString(beerDto);
+        System.out.println(jsonData);
+    }
+}
